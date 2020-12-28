1d83a5fd42915a76e11d05f6f349b3d177631b28
Graphity <rohood3@gmail.com>
2020-12-28 10:53:08 +0400

    reorganize

--- firstTask/SabaUmikashvili/src/calculator/service/impl/CalculatorService.java
+++
@@ -1,9 +0,0 @@
-package calculator.service.impl;
-
-public interface CalculatorService {
-    void sum(int x, int y);
-    void sum(float x, float y);
-
-    void devide(int x, int y);
-    void devide(float x, float y);
-}
